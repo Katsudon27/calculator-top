35f537a3382f1016abbb922032d2e93d0eafb7f6
Katsudon27 <katsudon1127@gmail.com>
2026-04-20 15:26:16 +1000

    Add functions to handle user input for basic math operations

--- javascript.js
+++ javascript.js
@@ -30,14 +30,64 @@ function operate(num1, operator, num2){
 let number1, operator, number2;
 
 const digitBtns = document.getElementsByClassName("digit");
+const operatorBtns = document.getElementsByClassName("operators");
+const equalBtn = document.getElementById("equal-btn");
+
+const miniText = document.getElementById("mini-text");
 const mainText = document.getElementById("main-text");
 
 const handleDigitInput = (text) => {
-    mainText.textContent = text;
+    mainText.textContent += text;
 }
+
+const handleOperatorInput = (text) => {
+    mainText.textContent += " ";
+    mainText.textContent += text;
+    mainText.textContent += " ";
+}
+
+const handleEqualInput = () => {
+    miniText.textContent = mainText.textContent;
+    const values = mainText.textContent.split(" ");
+    number1 = Number(values[0]);
+    console.log(values[1]);
+
+    switch (values[1]){
+        case "+":
+            operator = "+";
+            break;
+        case "−":
+            operator = "-";
+            break;
+        case "×":
+            operator = "*";
+            break;
+        case "÷":
+            operator = "/";
+            break;
+    }
+
+    number2 = Number(values[2]);
+    console.log(number1);
+    console.log(number2);
+    console.log(operator);
+    mainText.textContent = operate(number1, operator, number2);
+}
+
+
 
 Array.prototype.forEach.call(digitBtns, function(digitBtn) {
     digitBtn.addEventListener("click", () => {
         handleDigitInput(digitBtn.textContent);
     });
 });
+
+Array.prototype.forEach.call(operatorBtns, function(operatorBtn) {
+    operatorBtn.addEventListener("click", () => {
+        handleOperatorInput(operatorBtn.textContent);
+    });
+});
+
+equalBtn.addEventListener("click", () => {
+    handleEqualInput();
+})
