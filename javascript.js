fbce4f1a6e568d349702906c1cebb475eafb2f36
Katsudon27 <katsudon1127@gmail.com>
2026-04-22 10:39:55 +1000

    Refactor for code readability + comments

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Functions for math operations
 function add(num1, num2){
     return num1 + num2;
 }
@@ -27,32 +28,17 @@ function operate(num1, operator, num2){
     }
 }
 
-let number1 = "";
-let operator = ""; 
-let number2 = "";
+//Functions to handle user inputs
 
-const digitBtns = document.getElementsByClassName("digit");
-const operatorBtns = document.getElementsByClassName("operators");
-const equalBtn = document.getElementById("equal-btn");
-const clearBtn = document.getElementById("clear-btn");
-const decimalBtn = document.getElementById("decimal-btn");
-const backspaceBtn = document.getElementById("backspace-btn");
-
-const miniText = document.getElementById("mini-text");
-const mainText = document.getElementById("main-text");
-
-let operatorSet = false;
-let isEvaluated = false;
-let containDecimal = false;
-
-const handleDigitInput = (text) => {
+function handleDigitInput(text) {
     if (isEvaluated){
         clearDisplay();
     }
     mainText.textContent += text;
 }
 
-const handleOperatorInput = (text) => {
+function handleOperatorInput(text) {
+    //Checks if the current expression can be evaluated
     if (parseExpression() === true){
         evaluateExpression(true);
     }
@@ -66,6 +52,68 @@ const handleOperatorInput = (text) => {
     }
 }
 
+function addDecimal(text) {
+    //Checks if the current number already contains a decimal
+    if (containDecimal === false && !isEvaluated){
+        mainText.textContent += text;
+        containDecimal = true;
+    }
+}
+
+function handleBackspace() {
+    if (!isEvaluated){
+        //Check if the character to be removed is an operator
+        let check = isNaN(mainText.textContent.trim().slice(mainText.textContent.length - 1));
+        
+        if(check){
+            operatorSet = false;
+        }
+
+        mainText.textContent = mainText.textContent.slice(0, mainText.textContent.length - 1).trim();
+        mainText.textContent += " ";
+    }
+}
+
+function handleEqualInput() {
+    if (parseExpression()){
+        evaluateExpression();
+        isEvaluated = true;
+    }
+}
+
+//Function for handling keyboard inputs
+function checkKeyInput(event){
+    const isNumber = isFinite(event.key) && event.key !== ' ';
+
+    if (isNumber){
+        handleDigitInput(event.key);
+    }else{
+        switch (event.key){
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                handleOperatorInput(event.key);
+                break;
+            case "Enter":
+                handleEqualInput();
+                break;
+            case ".":
+                addDecimal(event.key);
+                break;
+            case "Backspace":
+                handleBackspace();
+                break;
+            case "Delete":
+                clearDisplay();
+                break;
+        }
+    }
+}
+
+//Helper functions for calculator logic
+
+//Function that parses the expression and assigns values to variables
 function parseExpression() {
     const values = mainText.textContent.trim().split(" ");
     if (values.length === 3 && !isNaN(values[0]) && !isNaN(values[2])){
@@ -102,11 +150,7 @@ function parseExpression() {
     }
 }
 
-function round2Digits(num){
-    return Math.round(num * 100) / 100;
-}
-
-function evaluateExpression(multiOperator=false){
+function evaluateExpression(multiOperator=false) {
     miniText.textContent = mainText.textContent;
     if(multiOperator === true){
         number1 = round2Digits(operate(number1, operator, number2));
@@ -117,7 +161,11 @@ function evaluateExpression(multiOperator=false){
     }
 }
 
-function clearDisplay(){
+function round2Digits(num) {
+    return Math.round(num * 100) / 100;
+}
+
+function clearDisplay() {
     miniText.textContent = "";
     mainText.textContent = "";
     number1 = "";
@@ -127,61 +175,26 @@ function clearDisplay(){
     isEvaluated = false;
 }
 
-function addDecimal(text){
-    if (containDecimal === false && !isEvaluated){
-        mainText.textContent += text;
-        containDecimal = true;
-    }
-}
+// Initialisation of variables 
+let number1 = "";
+let operator = ""; 
+let number2 = "";
 
-function handleBackspace(){
-    if (!isEvaluated){
-        let check = isNaN(mainText.textContent.trim().slice(mainText.textContent.length - 1));
-        mainText.textContent = mainText.textContent.slice(0, mainText.textContent.length - 1).trim();
-        mainText.textContent += " ";
+const digitBtns = document.getElementsByClassName("digit");
+const operatorBtns = document.getElementsByClassName("operators");
+const equalBtn = document.getElementById("equal-btn");
+const clearBtn = document.getElementById("clear-btn");
+const decimalBtn = document.getElementById("decimal-btn");
+const backspaceBtn = document.getElementById("backspace-btn");
 
-        if(check){
-            operatorSet = false;
-        }
-    }
-}
+const miniText = document.getElementById("mini-text");
+const mainText = document.getElementById("main-text");
 
-const handleEqualInput = () => {
-    if (parseExpression()){
-        evaluateExpression();
-        isEvaluated = true;
-    }
-}
+let operatorSet = false;
+let isEvaluated = false;
+let containDecimal = false;
 
-function checkKeyInput(event){
-    const isNumber = isFinite(event.key) && event.key !== ' ';
-
-    if (isNumber){
-        handleDigitInput(event.key);
-    }else{
-        switch (event.key){
-            case "+":
-            case "-":
-            case "*":
-            case "/":
-                handleOperatorInput(event.key);
-                break;
-            case "Enter":
-                handleEqualInput();
-                break;
-            case ".":
-                addDecimal(event.key);
-                break;
-            case "Backspace":
-                handleBackspace();
-                break;
-            case "Delete":
-                clearDisplay();
-                break;
-        }
-    }
-}
-
+//Attach event listeners for different types of inputs
 Array.prototype.forEach.call(digitBtns, function(digitBtn) {
     digitBtn.addEventListener("click", () => {
         handleDigitInput(digitBtn.textContent);
